997b0463ab57235d869b686600a6563fa16b762c
JiMoon GIl <roadzmoon76@gmail.com>
2022-10-05 14:43:51 +0900

    init project

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Hello :)</h1>
+      <h1>Hello 2</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
